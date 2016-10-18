1e46661ec9dd588b44bf504634938930533ffb9f
mmhansen <hansen.mmb@gmail.com>
2016-10-18 13:53:49 -0400

    layout for admin controls

--- client/app/components/Admin/AdminPage.jsx
+++ client/app/components/Admin/AdminPage.jsx
@@ -1,15 +1,35 @@
 import React from 'react';
+
 //components
 
 class AdminPage extends React.Component {
+  constructor(){
+    super()
+    // content is a flag for stories or users
+    this.state = {
+      content: 'stories'
+    }
+  }
+  onClick(e){
+    e.preventDefault();
+    let get = e.target.name;
+    // make axios request with get
+  }
   render (){
     return (
-      <div className="container" id="login-page">
-   		<div className="row">
-   			<div className="col-sm-12 col-md-4 col-md-offset-4">
-          <h1>Admin Page</h1>
-   			</div>
-     	</div>
+      <div className="col-xs-12 col-sm-12 col-md-8 col-md-offset-2" id="admin">
+        <div className="panel panel-login">
+          <div className="panel-heading">
+            <button name="stories" onClick={this.onClick.bind(this)} className="btn btn-primary btn-lg">Stories</button>
+            <button name="users" onClick={this.onClick.bind(this)} className="btn btn-primary btn-lg">Users</button>
+          </div>
+          <hr />
+          <div className="panel-body">
+            <div>
+              <span>clicking on this will let you see the whole article</span> <span>shows username and date posted</span>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
