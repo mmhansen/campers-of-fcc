ee78ad672ca0da5c9c03e6e521216eb615842043
mmhansen <hansen.mmb@gmail.com>
2016-10-18 13:29:14 -0400

    simplify submit story page styling

--- client/app/components/Story/StoryPage.jsx
+++ client/app/components/Story/StoryPage.jsx
@@ -55,41 +55,54 @@ class StoryPage extends React.Component {
     let { story } = this.state
 
     return (
-      <div className="container-fluid" id="story-form">
-        <div className="row">
-          <div className="col-sm-12 col-md-6">
-            <form onSubmit={handleSubmit(this.onSubmit)} className="storyForm">
-              <h2>Create</h2>
-              <p>{time}</p>
-              <Field type="text" name="username" component={renderField} label="Name"
-                    props={{value: userFullName}} disabled />
-              <Field type="url" name="image" component={renderField} label="Image" />
-              <Field type="text" name="title" component={renderField} label="Title"
-                    placeholder="A short title for your submission" />
-              <div className={classnames('form-group', {'has-error': errorMessage})}>
-                <h3>Tell your story</h3>
-                <Editor
-                  className="form-control"
-                  data-placeholder="Tell people about yourself, how you got started with FCC, and what you hope to achieve. Or something."
-                  text={story}
-                  onChange={this.onChangeMediumEditor}
-                  options={{toolbar: {buttons: ['bold', 'italic', 'underline','anchor']}}}
-                />
-                {errorMessage && <div className="text-danger">{errorMessage}</div>}
-              </div>
-              <input type="submit" value="Post" className="btn btn-primary pull-right"/>
-            </form>
-          </div>
-          <div className="col-sm-12 col-md-3 col-md-offset-1 well well-sm">
-            <h2>Tips for greatness</h2>
-            <ol>
-              <li>Make sure to include X,Y,Z</li>
-              <li>Highlight text to apply formatting</li>
-              <li>Have fun</li>
-            </ol>
+      <div id="story-form" className="col-xs-12 col-sm-12 col-md-6 col-md-offset-3">
+
+          <div className="row">
+            {/* Content */}
+            <div className="col-sm-9">
+              <form onSubmit={handleSubmit(this.onSubmit)} className="storyForm">
+                <h2>Tell Your Coding Story</h2>
+                <hr />
+                <div className="row">
+                  <div className="col-sm-6">
+                    <h4>{userFullName}</h4>
+                  </div>
+                  <div className="col-sm-6">
+                    <h4>{time}</h4>
+                  </div>
+                </div>
+                <div className="row">
+
+                </div>
+                <Field type="url" name="image" component={renderField} placeholder="Include an image url for your header"/>
+                <Field type="text" name="title" component={renderField}
+                  placeholder="Title" />
+                <div className={classnames('form-group', {'has-error': errorMessage})}>
+
+                  <Editor
+                    className="form-control"
+                    data-placeholder="Double click on text to style."
+                    text={story}
+                    onChange={this.onChangeMediumEditor}
+                    options={{toolbar: {buttons: ['bold', 'italic', 'underline','anchor']}}}
+                    />
+                  {errorMessage && <div className="text-danger">{errorMessage}</div>}
+                </div>
+                <input type="submit" value="Post" className="btn btn-primary pull-right"/>
+              </form>
+            </div>
+            {/* Tips */}
+            <div className="col-sm-3">
+              <h2>Tips</h2>
+              <ol>
+                <li>Make sure to include X,Y,Z</li>
+                <li>Highlight text to apply formatting</li>
+                <li>Have fun</li>
+                <li>Tell people about yourself, how you got started with FCC, and what you hope to achieve. Or something. </li>
+              </ol>
+            </div>
           </div>
         </div>
-      </div>
     )
   }
 }
