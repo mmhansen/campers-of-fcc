be068e95edfbe97650662126ec7de1824196d108
Oussama Bouguerne <oussama.bouguerne@metaworks.xyz>
2016-10-30 17:51:07 +0100

    Add a footer to the app

---
+++ client/app/components/common/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const Footer = () => {
+  return (<footer className="container-fluid text-center">
+    <div className="row">
+      <p>Humans of FreeCodeCamp &copy;2016</p>
+    </div>
+  </footer>)
+}
+
+export default Footer
