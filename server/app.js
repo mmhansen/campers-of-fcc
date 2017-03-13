29e19cb23e03a2d9d6a3d521adb649933de24653
Maxim <mkarabashev@gmail.com>
2017-03-13 21:46:34 +0200

    webpack production configuration

--- server/app.js
+++ server/app.js
@@ -29,19 +29,20 @@ console.log(`${config.name} config running on port ${config.port}`)
 if (process.env.NODE_ENV !== 'test'){
   app.use(logger('dev'));
 }
+app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(favicon(path.join(__dirname, '../client/public', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, '../client/public')));
 /*
- * Routes 
+ * Routes
  */
 auth(app);
 admin(app);
 content(app);
 
 app.get('*', function (request, response){
-  response.sendFile(path.resolve(__dirname, '../client/public', 'index.html'))
+  response.sendFile(path.resolve(__dirname, '../client/public/assets', 'index.html'))
 })
 
 /*
